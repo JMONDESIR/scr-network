fa2b89dab7122212bc51bae232768a630d496820
Joel Mondesir <joey.m.dev@gmail.com>
2024-09-26 17:07:48 -0500

    special textbox added to project

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -3,14 +3,19 @@ import React from 'react';
 import './Banner.css';
 
 const Banner = ({ title, image, altText }) => {
-  return (
-    <div className="banner-container"
-      style={{ backgroundImage: `url(${image})` }}
-      alt={altText} >
-      <div className="banner-polygon">
-        <h2 className="banner-sm-title">{title}</h2>
-      </div>
-    </div>
+    return (
+<div className="banner-container">
+  <div
+    className="banner-image"
+    style={{ backgroundImage: `url(${image})` }}
+    alt={altText}
+  >
+  </div>
+  <div className="banner-polygon">
+    <h2 className="banner-sm-title">{title}</h2>
+    
+  </div>
+</div>
   );
 };
 
