bd348834f95245d4baf91b223c25c1a6f56b17bf
Joel Mondesir <joey.m.dev@gmail.com>
2024-09-26 11:11:21 -0500

    slant box text added

--- src/components/headerLg/HeaderLg.js
+++ src/components/headerLg/HeaderLg.js
@@ -7,14 +7,28 @@ const HeaderLg = () => {
 
           <section className="box-inner box-1">
             <h2 className="box-title">SERVERS</h2>
+            <div className='box-text-cont'>
+              <p className='box-desc'>
+              We fix, maintain, & setup all makes and models of servers.
+              </p>
+            </div>
           </section>
 
           <section className="box-inner box-2">
               <h2 className="box-title">NETWORKING</h2>
+              <div className='box-text-cont'>
+              <p className='box-desc'>
+                Everything from home wireless routers to enterpise grade networks we can help you get connected.
+                </p>
+              </div>
           </section>
 
           <section className="box-inner box-3">
               <h2 className="box-title">COMPUTERS</h2>
+              <div className='box-text-cont'>
+              <p className='box-desc'>
+                From upgrades to virus removal, we can help up with your computers.                </p>
+              </div>
           </section>
           
         </div>
