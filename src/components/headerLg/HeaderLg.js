717f66e365dc8e480744279872f7898cb7ccf96c
Joel Mondesir <joey.m.dev@gmail.com>
2024-10-23 13:45:20 -0500

    improved visibility of large banner title

--- src/components/headerLg/HeaderLg.js
+++ src/components/headerLg/HeaderLg.js
@@ -6,29 +6,33 @@ const HeaderLg = () => {
         <div className="slant-box-container">
 
           <section className="box-inner box-1">
+          <div className="inner-box-align">
             <h2 className="box-title">SERVERS</h2>
             <div className='box-text-cont'>
-              <p className='box-desc'>
-              We fix, maintain, & setup all makes and models of servers.
+              <p className='box-desc'>We fix, maintain, & setup all makes and models of servers.
               </p>
+            </div>
             </div>
           </section>
 
           <section className="box-inner box-2">
+          <div className="inner-box-align">
               <h2 className="box-title">NETWORKING</h2>
               <div className='box-text-cont'>
-              <p className='box-desc'>
-                Everything from home wireless routers to enterpise grade networks we can help you get connected.
+              <p className='box-desc'>Everything from home wireless routers to enterpise grade networks we can help you get connected.
                 </p>
+              </div>
               </div>
           </section>
 
           <section className="box-inner box-3">
+            <div className="inner-box-align">
               <h2 className="box-title">COMPUTERS</h2>
               <div className='box-text-cont'>
-              <p className='box-desc'>
-                From upgrades to virus removal, we can help up with your computers.</p>
+                <p className='box-desc'>From upgrades to virus removal, we can help up with your computers.</p>
               </div>
+            </div>
+
           </section>
           
         </div>
